37edb6dcba3275606f42feb38d2085100d3abb69
ssabhijith2000 <abhijith.ss@inapp.com>
2022-07-18 17:37:17 +0530

    python asst

---
+++ algorithms/asst2.py
@@ -0,0 +1,43 @@
+numberList = [1,2,6,4,9,6,22]
+
+#1. sort in asdcending order and print the first element in the list
+numberList.sort()
+print(numberList[0])
+
+#2. sort the second largest number in the list
+print(numberList[-2])
+
+#3. to print odd and even nnumbers seperately
+numbersList =  [i for i in range(11)]
+oddNumbers=numbersList[1:11:2]
+evenNumbers=numbersList[2:11:2]
+
+print(evenNumbers,oddNumbers)
+
+#4. program to reverse  a list
+numberList.reverse()
+print(numberList)
+
+#5. Program to print all odd numbers from 1-50
+print([x for x in range(1,50,2)])
+
+#6. prgogram to count Even and Odd numbers in a list
+countEven= 0
+countOdd = 0
+for x in numberList:
+    if x%2 == 0:
+        countEven= countEven+1
+    else:
+        countOdd = countOdd + 1
+print("Number of Even numbers is {0} and Number of Odd numbers is {1}".format(countEven,countEven))
+ 
+#regexp
+
+#1. remove zeroes from an ip address
+IP_ADDRESS = "216.08.094.196"
+print(IP_ADDRESS.replace('0',''))
+
+#2. match a string that has an a followed by anything ending in b
+import re 
+text= "Hi this is arnab"
+print(re.findall(".*[Aa].*b$", text))
--- text-format.py
+++ text-format.py
@@ -1,3 +1,49 @@
-name = '{} is my country. All Indians are my brothers and sisters.'.format('India')
-print(name)
-print(name.count('India'))
+import re
+# name = '{} is my country. All Indians are my brothers and sisters.'.format('India')
+# print(name)
+# print(name.count('India'))
+
+#regular expressions
+
+#matching an email within a string using special sequence
+myString = "my email is abhi@abhi.com hopw you will note it down"
+
+#regular expression to match email
+#check for non space chars before and after '@'
+regex = '\S+@\S+'
+x= re.findall(regex, myString)
+print(x)
+
+#append to add a value to the end of the list 
+studentsAge = []
+studentsAge.append(16)
+studentsAge.append("hi")
+print(studentsAge)
+
+del studentsAge[-1]
+print(studentsAge)
+
+#combine two lists using extend()
+
+studentsName = ['Anu', 'Binu', 'Sinu']
+studentsAge.extend(studentsName)
+print(studentsAge)
+
+#to check if a variable is in the list
+print('Anu' in studentsName)
+
+#get the number of items in the list
+print(len(studentsName))
+
+#sort the list either alphabetically or  numerically
+studentsAge.sort()
+print(studentsAge)
+
+#list concatenation using + operator
+print(studentsName + studentsName)
+print(studentsName)
+
+#list duplication/multiplication using * operator
+print(studentsName*3)
+print(studentsName)
+
