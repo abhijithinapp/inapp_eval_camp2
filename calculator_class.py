d6a3f172ae9fae6ea0c2633c39c79a384c04953a
ssabhijith2000 <abhijith.ss@inapp.com>
2022-07-21 12:44:29 +0530

    updated calculator

--- calculator_class.py
+++ calculator_class.py
@@ -10,18 +10,20 @@ class Calculator:
         Calculator.input2 = input2
 
     def add(self):
-        print("Sum is: ",Calculator.input1 + Calculator.input2)
+        print("Sum of",Calculator.input1,"and", Calculator.input2, "is", Calculator.input1 + Calculator.input2)
 
     def multiply(self):
-        print("Product is: ",Calculator.input1 * Calculator.input2)               
+        print("Product of", Calculator.input1,"and", Calculator.input2, "is",Calculator.input1 * Calculator.input2)               
         
     def divide(self):
         if Calculator.input2 == 0 :
             print("Division not possible due to division by zero error")
         else: 
-            print("Qoutient is: ",Calculator.input1 / Calculator.input2)               
+            print("Qoutient of", Calculator.input1,"and", Calculator.input2, "is",Calculator.input1 / Calculator.input2)               
 
-calculation1 = Calculator(1,0)
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+calculation1 = Calculator(num1,num2)
 calculation1.add()
 calculation1.multiply()
 calculation1.divide()
