af456f7b7e253f39e31a24aa580d3f7c56643cd4
abhijithinapp <109217423+abhijithinapp@users.noreply.github.com>
2022-07-27 03:01:12 +0530

    complted db phonebook assignment

--- contacts_app_database.py
+++ contacts_app_database.py
@@ -1,8 +1,8 @@
-import os
-from sys import flags
+from ast import Break
 import pyodbc
-conString = 'Driver={SQL Server};Server=CPU1213\SQLEXPRESS;Database=camp2;Trusted_Connection=yes;'
-TABLE_EXISTS_
+import re
+conString = 'Driver={SQL Server};Server=ABHIJITH\SQLEXPRESS;Database=camp2;Trusted_Connection=yes;'
+TABLE_EXISTS_ERROR_CODE = '42S01'
 class Utils:
     def getInt():
         while(True):
@@ -13,143 +13,198 @@ class Utils:
                 print("Enter a valid number")
                 continue
 
+    def getNumber():
+        while(True):
+                value = input()
+                if re.match(r'\+[0-9]*', value):
+                    return value
+                else:
+                    print("Enter a valid number")
+                    continue
+
 def initialiseContactsDB():
-    myconn = pyodbc.connect(conString)
-    myCursor = myconn.cursor()
-    try:     
-        myCursor.execute('''CREATE TABLE phonebook(
-            id INT IDENTITY PRIMARY KEY,
-            name VARCHAR(50),
-            phone INT ,
-            )''')
-    except Exception as e:
-        if e.args[0] == ''
-        print("Cannot create the table")
-        print(f"{type(e).__name__} was occured,")
-        print(e)
-    else : 
-        print("No existing database found. Creating new one..")
+    while(True):
+        try:
+            print("Connecting to database..")
+            myconn = pyodbc.connect(conString)
+            myCursor = myconn.cursor()
+            try:     
+                myCursor.execute('''CREATE TABLE phonebook(
+                    id INT IDENTITY PRIMARY KEY,
+                    name VARCHAR(50) NOT NULL,
+                    phone VARCHAR(20) NOT NULL UNIQUE,
+                    )''')
+            except Exception as e:
+                if e.args[0] == TABLE_EXISTS_ERROR_CODE:
+                    print("Found Phone book Database. Acessing the phone book..")
+                else: 
+                    print('Something went wrong')
+                    print(f"{type(e).__name__} was occured,")
+            else : 
+                print("No existing database found. Creating new one..")
+            break
+        except:
+            print("Could not connect to databse")
+            continue
     myconn.commit()
     myconn.close()
+
+def searchByNumber():
+    print("Enter Number: ")
+    number = Utils.getNumber()
+    while(True):
+        try:
+            print("Connecting to database..")
+            myconn = pyodbc.connect(conString)
+            myCursor = myconn.cursor()
+            try:
+                print("Searching contact from the database")
+                myCursor.execute('SELECT * FROM phonebook WHERE phone = ?;',(number))           
+            except Exception as e:
+                print('Could not search from database')
+                print(e)
+            else:
+                contact = myCursor.fetchall()
+                if len(contact)>0:
+                    print('Contact Found..')
+                    for _, name, phone  in contact:
+                        print(f'Name: {name}\nPhone: {phone}')
+                else:
+                    print('This number does not exist in database')
+            break
+        except:
+            print("Could not connect to databse")
+            continue
+    myconn.commit()
+    myconn.close()
+
+
+def addContact():
+    print("Enter Name: ")
+    name = input()
+    print("Enter Phone NUmber: ")
+    number = Utils.getNumber()
+    while(True):
+        try:
+            print("Connecting to database..")
+            myconn = pyodbc.connect(conString)
+            myCursor = myconn.cursor()
+            try:
+                print("Adding contact to the database")
+                myCursor.execute('INSERT INTO phonebook(name,phone) VALUES(?,?);',(name, number))           
+            except Exception as e:
+                print('Could not write to database')
+                print(e)
+            else:
+                print('Contact added successfully..')
+            break
+        except:
+            print("Could not connect to databse")
+            continue
+    myconn.commit()
+    myconn.close()
+       
+
+def listAllContacts():
+    while(True):
+        try:
+            print("Connecting to database..")
+            myconn = pyodbc.connect(conString)
+            myCursor = myconn.cursor()
+            try:
+                print("Listing All Contacts from database")
+                myCursor.execute('SELECT * FROM phonebook ;')           
+            except Exception as e:
+                print('Could not list data from database')
+                print(e)
+            else:
+                contact = myCursor.fetchall()
+                if len(contact)>0:
+                    print('Contact are..')
+                    for _, name, phone  in contact:
+                        print(f'Name: {name}\nPhone: {phone}\n')
+                else:
+                    print('No contacts in database')
+            break
+        except:
+            print("Could not connect to databse")
+            continue
+    myconn.commit()
+    myconn.close()
+
+def searchByName():
+    print("Enter Name: ")
+    name = input()
+    while(True):
+        try:
+            print("Connecting to database..")
+            myconn = pyodbc.connect(conString)
+            myCursor = myconn.cursor()
+            try:
+                print("Searching contact from the database")
+                myCursor.execute('SELECT * FROM phonebook WHERE name = ?;',(name))           
+            except Exception as e:
+                print('Could not search from database')
+                print(e)
+            else:
+                contact = myCursor.fetchall()
+                if len(contact)>0:
+                    print('Contact Found..')
+                    for _, name, phone  in contact:
+                        print(f'Name: {name}\nPhone: {phone}\n')
+                else:
+                    print('This name does not exist in database')
+            break
+        except:
+            print("Could not connect to databse")
+            continue
+    myconn.commit()
+    myconn.close()
+
+def deleteContact():
+    print("Enter Number: ")
+    number = Utils.getNumber()
+    while(True):
+        try:
+            print("Connecting to database..")
+            myconn = pyodbc.connect(conString)
+            myCursor = myconn.cursor()
+            try:
+                print("Deleting contact from the database")
+                myCursor.execute('DELETE FROM phonebook WHERE phone = ?;',(number))           
+            except Exception as e:
+                print('Could not delete from database')
+                print(e)
+            else:
+                if myCursor.rowcount>0:
+                    print('Contact deleted successfully..')
+                else:
+                    print('This number does not exist in database')
+            break
+        except:
+            print("Could not connect to databse")
+            continue
+    myconn.commit()
+    myconn.close()
+
 initialiseContactsDB()
-# def searchByNumber(enteredNumber, isAddContact):
-#     if os.path.exists("contacts.txt"):
-#             myFile = open("contacts.txt", "r")
-#             contacts = myFile.readlines()
-#             myFile.close()
-#             flag = 0
-#             for contact in contacts:
-#                 name,number = contact.strip().split(" : ")
-#                 number= int(number)
-#                 if number == enteredNumber:
-#                     print("Contact Already Exists" if isAddContact else "Contact Found")
-#                     print("" if isAddContact else f"""Name: {name} 
-# Phone number: {number}\n2""") 
-#                     flag = 1
-#                     return True
-#             if flag == 0:
-#                 print("" if isAddContact else "No such Contact exists")
-#                 return False
-#     else:
-#         print("Contacts database not found. Please wait for a while and check again.")
 
-# def addContact():
-#         print("Enter Name: ")
-#         name = Utils.getName()
-#         print("Enter Phone NUmber: ")
-#         number = Utils.getInt()
-#         if os.path.exists("contacts.txt"):
-#             if not searchByNumber(number, True):
-#                 myFile = open("contacts.txt", "a")
-#                 myFile.write(name.strip()+" : "+str(number)+"\n")
-#                 print("Contact added.")
-#                 myFile.close()
-#             else:
-#                 print("Contact already exists")
-#         else:
-#             print("Contacts database not found. Please wait for a while and check again.")
-
-# def listAllContacts():
-#     if os.path.exists("contacts.txt"):
-#             myFile = open("contacts.txt", "r")
-#             contacts = myFile.readlines()
-#             myFile.close()
-#             contacts.sort()
-#             for contact in contacts:
-#                 name,number = contact.strip().split(" : ")
-#                 number = int(number)
-#                 print(f"""Name: {name} 
-# Phone number: {number}\n""") 
-#     else:
-#         print("Contacts database not found. Please wait for a while and check again.")
-
-# def searchByName(enteredName):
-#     if os.path.exists("contacts.txt"):
-#             myFile = open("contacts.txt", "r")
-#             contacts = myFile.readlines()
-#             myFile.close()
-#             flag = 0
-#             for contact in contacts:
-#                 name,number = contact.strip().split(" : ")
-#                 number = int(number)
-#                 if name == enteredName:
-#                     print("Contact Found")
-#                     print(f"""Name: {name} 
-# Phone number: {number}\n2""") 
-#                     flag = 1
-#             if flag == 0:
-#                 print("No such Contact exists")
-#     else:
-#         print("Contacts database not found. Please wait for a while and check again.")
-
-# def deleteContact(enterednumber):
-#     if os.path.exists("contacts.txt"):
-#             myFile = open("contacts.txt", "r")
-#             contacts = myFile.readlines()
-#             myFile.close()
-#             flag = 0
-#             for contact in contacts:
-#                 name,number = contact.strip().split(" : ")
-#                 number = int(number)
-#                 if number == enterednumber:
-#                     contacts.remove(contact)
-#                     print(f"Contact with name {name} Deleted")
-#                     flag = 1
-#             myFile = open("contacts.txt", "w")
-#             contacts = myFile.writelines(contacts)
-#             myFile.close()
-#             if flag == 0:
-#                 print("No such Contact exists")
-#     else:
-#         print("Contacts database not found. Please wait for a while and check again.")
-
-# initialiseContactsDB()
-# while(True):
-#     print("""Enter your required option:
-#     1. Add Contact
-#     2. List Contact
-#     3. Search by Name
-#     4. Search by Number
-#     5. Delete a number
-#     6. exit
-#     """)
-#     option = Utils.getInt()
-#     match(option):
-#         case 1: addContact()
-#         case 2: listAllContacts()
-#         case 3: 
-#             print("Enter name:")
-#             name = Utils.getName()
-#             searchByName(name)
-#         case 4: 
-#             print("Enter Number: ")
-#             number = Utils.getInt()
-#             searchByNumber(number,False)
-#         case 5: 
-#             print("Enter Number: ")
-#             number = Utils.getInt()
-#             deleteContact(number)
-#         case 6: break
-#         case _: 
-#             print("Invalid option")
-#             continue
+while(True):
+    print("""Enter your required option:
+    1. Add Contact
+    2. List Contact
+    3. Search by Name
+    4. Search by Number
+    5. Delete a number
+    6. exit
+    """)
+    option = Utils.getInt()
+    match(option):
+        case 1: addContact()
+        case 2: listAllContacts()
+        case 3: searchByName()
+        case 4: searchByNumber()
+        case 5: deleteContact()
+        case 6: break
+        case _: print("Invalid option")
+            
