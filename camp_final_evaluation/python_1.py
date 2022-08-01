afc0b04ebc9c9118902522d07085306032eaeea1
abhijithinapp <109217423+abhijithinapp@users.noreply.github.com>
2022-08-01 23:26:05 +0530

    complted python1 eval

--- camp_final_evaluation/python_1.py
+++ camp_final_evaluation/python_1.py
@@ -1,9 +1,7 @@
-from msilib.schema import Property
-import numbers
+from itertools import product
 from random import randint
 import re
-from abc import ABC, abstractmethod
-from tkinter.font import names
+from prettytable import PrettyTable
 
 class Utils:
     def getInt(*msg):
@@ -22,14 +20,23 @@ class Utils:
             else:
                 print("Enter a valid number in range 0-6")
                 continue
+    def getName(*msg):
+        while(True):
+            value = input(*msg)
+            if len(value) > 0:
+                return value
+            else:
+                print("Product name should be minimum 1 character long")
+                continue
 
-MAX_PRODUCTS_IN_EACH_CATEGORY = Utils.getInt('Enter maximum value in each category')
+MAX_PRODUCTS_IN_EACH_CATEGORY = Utils.getInt('Enter maximum value in each category: ')
 products = dict()
-class Category(ABC): 
+class Category(): 
     def __init__(self, products = 0):
         self.__numberOfProductsPresent = products
+    
     def incrementnumberOfProductsPresent(self):
-        self.__numberOfProductsPresent = self.numberOfProductsPresent +1
+        self.__numberOfProductsPresent = self.__numberOfProductsPresent +1
     
     @property
     def numberOfProductsPresent(self):
@@ -37,6 +44,7 @@ class Category(ABC):
 
 class Hygiene(Category):
     def __init__(self):
+        super().__init__()
         self.__code = 'HY'
     @property 
     def code(self):
@@ -44,6 +52,7 @@ class Hygiene(Category):
     
 class Health(Category):
     def __init__(self):
+        super().__init__()
         self.__code = 'HE'
     @property 
     def code(self):
@@ -51,6 +60,7 @@ class Health(Category):
 
 class Staples(Category):
     def __init__(self):
+        super().__init__()
         self.__code = 'ST'
     @property 
     def code(self):
@@ -58,6 +68,7 @@ class Staples(Category):
 
 class Sports(Category):
     def __init__(self):
+        super().__init__()
         self.__code = 'SP'
     @property 
     def code(self):
@@ -65,6 +76,7 @@ class Sports(Category):
 
 class Fashion(Category):
     def __init__(self):
+        super().__init__()
         self.__code = 'FA'
     @property 
     def code(self):
@@ -76,8 +88,8 @@ class validNameException(Exception):
 
 class Product:
     @property
-    def name(self):
-        return self.__name
+    def productName(self):
+        return self.__productName
 
     @property 
     def category(self):
@@ -95,17 +107,19 @@ class Product:
     def taxPercent(self):
         return self.__taxPercent
 
+    @property
+    def mrp(self):
+        return self.__mrp
     @property 
     def discount(self):
         return self.__discount
 
-    @name.setter 
-    def name(self, name):
-        if re.match('[a-zA-Z]{3,}', name):
-            self.__name = name
+    @productName.setter 
+    def productName(self, name):
+        if re.match('[a-zA-Z]{3,}$', name):
+            self.__productName = name
         else:
             raise validNameException('Name should be of atleast 3 letters and all characters')
-
 
     @category.setter 
     def category(self, category):
@@ -124,16 +138,13 @@ class Product:
     def taxPercent(self, taxPercent):
         self.__taxPercent = taxPercent
     
+    @mrp.setter
+    def mrp(self,mrp):
+        self.__mrp = mrp
+
     @discount.setter 
     def discount(self, discount):
         self.__discount = discount
-    
-    @name.setter 
-    def name(self, name):
-        if re.match('[a-zA-Z]{3,}', name):
-            self.__name = name
-        else:
-            raise validNameException('Name should be of atleast 3 letters and all characters')
 
 categories = {
     1: Hygiene(),
@@ -144,29 +155,100 @@ categories = {
 }
 
 def addProducts():
-    name = input("Enter name")
+
     categoryoption = Utils.getCategory('''Enter the number corresponding to category:
         1. Hygiene
         2. Health
         3. Staples
         4. Sports
-        5. Fashion''')
+        5. Fashion
+        ''')
     category = categories[categoryoption]
-    productCode = name[:2].upper()
-    categoryCode = category.code 
-    coderegex = '{categoryCode}{productCode}'
-    numberofCodes = len([s for s in list(products.keys()) if re.match(coderegex,s)])
-    lastThreeRandomValues = '{0:03d}'.format(randint(0,999))
-    code=f'{categoryCode}{productCode}{str(numberofCodes)}{lastThreeRandomValues}'
-    basicpice = Utils.getInt("enter price")
-    product = Product()
-    product.basicPrice=basicpice
-    product.productCode = code
-    products.update({code: product})
-    print(list(products.keys()))
-    
+    if category.numberOfProductsPresent < MAX_PRODUCTS_IN_EACH_CATEGORY:
+       
+        product = Product() 
+        while(True):
+            try:
+                name = Utils.getName("Enter name: ")
+                product.productName = name
+            except Exception as e:
+                print (e)
+            else: break
 
+        #codeblock to generate product code
+        productCodeWord = name[:2].upper()
+        categoryCodeWord = category.code 
+        coderegex = f'^{categoryCodeWord}{productCodeWord}'
+        numberofProductsWithSameCodeWord = len(list(filter(lambda x: re.match(coderegex,x),list(products.keys()))))
+        lastThreeRandomValues = '{0:03d}'.format(randint(0,999))
+        productCode=f'{categoryCodeWord}{productCodeWord}{str(numberofProductsWithSameCodeWord)}{lastThreeRandomValues}'
+        
+        product = Product()
+        taxPercent = Utils.getInt("Enter tax percent: ")
+        product.taxPercent = taxPercent
+        product.productCode = productCode
 
+        while(True):
+            priceOption = Utils.getInt('''
+            Enter Price as:
+            1. Including Tax
+            2. Excluding tax''')
+
+            price = Utils.getInt("Enter Price: ")
+            match(priceOption):
+                case 1: 
+                    product.mrp = price
+                    product.basicPrice = round(((100-product.taxPercent)*price)/100,2)
+                    break
+                case 2:
+                    product.mrp = round(((100+product.taxPercent)*price)/100,2)
+                    product.basicPrice = price
+                    break
+                case _:
+                    print("Enter valid option")
+
+        while(True):
+            discountOption = Utils.getInt('''
+            Enter Discount as:
+            1. Percentage
+            2. Absolute price''')
+
+            discount = Utils.getInt("Enter Discount: ")
+            match(discountOption):
+                case 1: 
+                    product.discount = discount
+                    break
+                case 2:
+                    product.discount = round(((product.mrp)*discount)/100,2)
+                    break
+                case _:
+                    print("Enter valid option")
+
+        products.update({productCode: product})
+        category.incrementnumberOfProductsPresent()
+        print('Entered the product to database' )
+    else:
+        print('Cannot Enter the product as the maximum products in this category exceeded')
+
+def listAllProducts():
+    if (len(products))>0:
+        myTable = PrettyTable(["Product Code" ,"tax%", "Base Price", "Tax Amount", "MRP", "Discout Percent", "Discount Amount", "Selling Price"])
+        print(products.items())
+        for productcode,product in products.items():
+            # productName = product.productName
+            taxPercent = product.taxPercent
+            taxAmount = round((product.taxPercent*product.basicPrice)/100,2)
+            basePrice = product.basicPrice
+            mrp = product.mrp
+            discountPercent = round((product.discount*product.basicPrice)/100,2)
+            discountAmount = product.discount
+            sellingPrice = product.mrp - product.discount
+            myTable.add_row([productcode,taxPercent,basePrice,taxAmount, mrp, discountPercent, discountAmount,sellingPrice])
+        print(myTable)
+        
+
+    else: 
+        print("No products found")
 
 
 while(True):
@@ -178,7 +260,6 @@ while(True):
     option = Utils.getInt()
     match(option):
         case 1: addProducts()
-            
-
+        case 2: listAllProducts()
         case _: print("Invalid option")
             
